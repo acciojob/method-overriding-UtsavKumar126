ec09ee24ed09fa79c1954ab41761330d3887aa33
utsav <guptautsavkumar126@gmail.com>
2023-01-15 23:01:12 +0530

    ans added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    B b=new B();
+
+    String k= b.meth();
+    String c= b.meth();
+
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
