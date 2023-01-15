bb790e6a3ab899b7d3e12f4d0c2500a6b5e957b1
utsav <guptautsavkumar126@gmail.com>
2023-01-15 23:08:39 +0530

    new ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,15 @@ public class Main {
         String c= b.meth();
     }
 
-}
-class A{
-    public String meth(){
-        return "Invoking method from class A";
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
     }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
+
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
